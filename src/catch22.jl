743ed5f4a5c954c3c0ebca3f67d10a1ee231296e
brendanjohnharris <brendanjohnharris@gmail.com>
2021-03-01 09:17:28 +1100

    Capitalise name

--- src/catch22.jl
+++ src/catch22.jl
@@ -1,4 +1,4 @@
-module catch22
+module Catch22
 using catch22_jll
 using Libdl
 using NamedArrays
@@ -12,17 +12,18 @@ catch22_jll.__init__() # Initialise the c-library
 zscore(x::AbstractVector{Float64}) = (x .- Statistics.mean(x))./(Statistics.std(x))
 
 """
-    catch1(x::AbstractVector{Float64}, f::Symbol)
+    catch22(x::AbstractVector{Float64}, f::Symbol)
+    catch22(f::Symbol, x::AbstractVector{Float64})
 
 Evaluate the feature 'f' on the time series 'x'
 
 # Examples
 ```julia-repl
-x = catch22.testData[:test]
-catch1(x, ::DN_HistogramMode_5)
+x = Catch22.testData[:test]
+catch22(x, ::DN_HistogramMode_5)
 ```
 """
-function catch1(x::AbstractVector{Float64}, fName::Symbol)
+function catch22(x::AbstractVector{Float64}, fName::Symbol)
     if any(isinf.(x)) || any(isnan.(x))
         return NaN
     end
@@ -34,30 +35,29 @@ function catch1(x::AbstractVector{Float64}, fName::Symbol)
         ccall(dlsym(dlopen(ccatch22), fName), Cint, (Ptr{Array{Cdouble}},Cint), x, Int(size(x)[1]))
     end
 end
-catch1(fName::Symbol, x::AbstractVector) = catch1(x, fName)
-#catch1(x::Int, f::Symbol) = catch1(Float64.(x), f)
-export catch1
+catch22(fName::Symbol, x::AbstractVector) = catch22(x, fName)
+#catch22(x::Int, f::Symbol) = catch22(Float64.(x), f)
 
 
 
 """
     DN_HistogramMode_5(x::AbstractVector{Union{Float64, Int}}) # For example
-An alternative to 'catch1(...)'; specific features can be evaluated with functions called by their names
+An alternative to 'catch22(...)'; specific features can be evaluated with functions called by their names
 
 # Examples
 ```julia-repl
-x = catch22.testData[:test]
+x = Catch22.testData[:test]
 DN_HistogramMode_5(x)
 ```
 """
-DN_HistogramMode_5(x::AbstractVector) = catch1(x, :DN_HistogramMode_5)
+DN_HistogramMode_5(x::AbstractVector) = catch22(x, :DN_HistogramMode_5)
 # Do a feature manually for example
 export DN_HistogramMode_5
 
 # Then generate the rest
 for fName = featureNames[2:end]
     eval(quote
-        $fName(x::AbstractVector) = catch1(x, $(Meta.quot(fName))); export $fName
+        $fName(x::AbstractVector) = catch22(x, $(Meta.quot(fName))); export $fName
     end)
 end
 
@@ -65,25 +65,25 @@ end
 
 
 """
-    catch22_all(X::Union{Vector, Array})
+    catch22(X::Union{Vector, Array})
 Evaluate all features for a time series vector or the columns of the time series array.
 Features are returned as a NamedArray, which behaves as a base array but has rows labelled with feature names.
 
 # Examples
 ```julia-repl
-x = catch22.testData[:test]
+x = Catch22.testData[:test]
 DN_HistogramMode_5(x)
 ```
 """
-function catch22_all(x::AbstractVector)
-    f = NamedArray(catch1.((x,), featureNames))
+function catch22(x::AbstractVector)
+    f = NamedArray(catch22.((x,), featureNames))
     setnames!(f, String.(featureNames), 1)
     return f
 end
-function catch22_all(X::AbstractArray)
-    F = mapslices(catch22_all, X, dims=[1])
+function catch22(X::AbstractArray)
+    F = mapslices(catch22, X, dims=[1])
 end
-export catch22_all
+export catch22
 
 
 end
