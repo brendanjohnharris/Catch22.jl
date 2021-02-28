0d232b5af1299abea1189cd1b7b08a037fa7b518
brendanjohnharris <brendanjohnharris@gmail.com>
2021-02-28 11:54:01 +1100

    Add feature output types and change location shared library is opened in

--- src/testdata.jl
+++ src/testdata.jl
@@ -10,7 +10,9 @@ testNames = [
     :testSinusoid
 ]
 
-
+# -------------------------------- Load test timeseries into dict -------------------------------- #
 testData = Dict(testNames .=> map(x -> reduce(vcat, readdlm(normpath(joinpath(@__DIR__, "../test/testData", String(x)*".txt")), ' ', Float64, '\n')), testNames))
 
+
+# ---------------------------- Load expected feature outputs into dict --------------------------- #
 testOutput = Dict(testNames .=> map(x -> reduce(vcat, readdlm(normpath(joinpath(@__DIR__, "../test/testData", String(x)*"_output.txt")), ' ', Float64, '\n', comments=true)), testNames))
