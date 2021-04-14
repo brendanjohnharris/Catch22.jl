ef1fc168772a2f9564adce22f5a711e1faa675e1
BrendanJHarris <brendanjohnharris@gmail.com>
2021-04-14 16:43:37 +1000

    Tweak scaling script

--- test/scaling.jl
+++ test/scaling.jl
@@ -1,6 +1,8 @@
 using Catch22
 using Plots
 
+catch22(randn(1000))
+
 function timeCatch22(𝒳)
     t⃗ = [(@timed catch22(𝐱)) for 𝐱 ∈ 𝒳]
     ([x.time for x in t⃗], [x.bytes for x in t⃗])
@@ -12,6 +14,6 @@ t⃗, b⃗ = timeCatch22(𝒳);
 
 
 plot(N⃗, t⃗, scale=:log10, label=:none, color=:black, markersize=2, marker=:o, right_margin=15Plots.mm)
-plot!(xguide="Time-series Length", yguide="Time (s)", framestyle=:box, minorticks=true)
+plot!(xguide="Length", yguide="Time (s)", framestyle=:box, minorticks=true)
 
-plot!(twinx(), N⃗, b⃗, scale=:log10, label=:none, seriescolor=:red, markersize=2, markershape=:circle, markerstrokewidth=0, grid=:off, yguide="Memory (bytes)", foreground_color_guide=:red, minorticks=true)
+plot!(twinx(), N⃗, b⃗, scale=:log10, label=:none, seriescolor=:red, markersize=2, markershape=:circle, markerstrokewidth=0, grid=:off, yguide="Memory (B)", foreground_color_guide=:red, minorticks=true)
