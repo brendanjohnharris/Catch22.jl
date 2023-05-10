a6f5e7c670d16085e4f305740dbeb395f4e113f6
brendanjohnharris <brendanjohnharris@gmail.com>
2023-05-10 16:47:43 +1000

    Update plots

--- test/scaling.jl
+++ test/scaling.jl
@@ -13,7 +13,7 @@ N⃗ = Int.(round.(exp10.(0.9:0.1:5)));
 t⃗, b⃗ = timeCatch22(𝒳);
 
 gray = :gray50
-p = plot(N⃗, t⃗, scale=:log10, label=:none, color=:cornflowerblue, markerstrokecolor=:cornflowerblue, markersize=2, marker=:o, right_margin=15Plots.mm, ylims=(1e-4, 1e1), xlims=(1e1, 1e5), framestyle=:box)
+p = plot(N⃗, t⃗, scale=:log10, label=:none, color=:cornflowerblue, markerstrokecolor=:cornflowerblue, markersize=2, marker=:o, right_margin=15Plots.mm, ylims=(1e-4, 1e1), xlims=(1e1, 1e5), framestyle=:box, grid=:off)
 plot!(xguide="Time-series length (samples)", yguide="Time (s)", minorticks=true, yforeground_color_guide=:cornflowerblue, dpi=1200, background_color = :transparent, foreground_color_axis=gray, foreground_color_border=gray, foreground_color_text=gray, foreground_color_guide=gray, foreground_color_grid=gray)
 
 plot!(twinx(), N⃗, b⃗./(1024^2), scale=:log10, label=:none, color=:crimson, markersize=2, marker=:o, markerstrokewidth=0, grid=:off, yguide="Memory (MiB)", yforeground_color_guide=:crimson, minorticks=true, xticks=nothing, markerstrokecolor=:crimson, xlims=(1e1, 1e5), foreground_color_axis=gray,foreground_color_border=gray, foreground_color_text=gray, foreground_color_guide=gray, framestyle=:box)
