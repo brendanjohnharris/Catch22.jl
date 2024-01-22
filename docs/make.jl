c9b0fffa1029fdd6e589649f4078cd9e1e834785
brendanjohnharris <bhar9988@uni.sydney.edu.au>
2024-01-23 08:59:30 +1100

    Update compat

--- docs/make.jl
+++ docs/make.jl
@@ -2,24 +2,20 @@ using Catch22
 using Catch22.TimeseriesFeatures
 using Documenter
 
-DocMeta.setdocmeta!(Catch22, :DocTestSetup, :(using Catch22); recursive=true)
+DocMeta.setdocmeta!(Catch22, :DocTestSetup, :(using Catch22); recursive = true)
 
 makedocs(;
-    modules=[Catch22, TimeseriesFeatures],
-    authors="brendanjohnharris <brendanjohnharris@gmail.com> and contributors",
-    repo="https://github.com/brendanjohnharris/Catch22.jl/blob/{commit}{path}#{line}",
-    sitename="Catch22.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        edit_link="main",
-        assets=String[]
-    ),
-    pages=[
-        "Home" => "index.md",
-    ]
-)
+         modules = [Catch22, TimeseriesFeatures],
+         authors = "brendanjohnharris <brendanjohnharris@gmail.com> and contributors",
+         repo = "https://github.com/brendanjohnharris/Catch22.jl/blob/{commit}{path}#{line}",
+         sitename = "Catch22.jl",
+         format = Documenter.HTML(;
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  edit_link = "main",
+                                  assets = String[]),
+         pages = [
+             "Home" => "index.md",
+         ])
 
-deploydocs(
-    repo="github.com/brendanjohnharris/Catch22.jl.git",
-    devbranch="main"
-)
+deploydocs(repo = "github.com/brendanjohnharris/Catch22.jl.git",
+           devbranch = "main")
